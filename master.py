b6b7612a2a85c3ac26b05ce158c89147e4ca61b3
atlroc99 <tyaniceluv89@gmail.com>
2021-09-03 08:12:18 -0400

    initial edit

--- master.py
+++ master.py
@@ -1,23 +1,22 @@
 import requests
 import json
 import os
+from dotenv import load_dotenv
 
+# pip install -r requirements.txt
 master = {'master' }
 
 # request to get all the artifact inside particular repo path
 url = "http://localhost:8082/artifactory/api/search/aql"
-
-
 payload = "items.find(\n{\n  \"repo\": \"firmware-snapshot-local\",\n  \"path\": {\n    \"$match\": \"driverx/fwex\"\n  },\n  \"$and\":[\n  {\"name\": {\n    \"$nmatch\": \"107-02-001\"\n  }},\n   {\"name\": {\n    \"$nmatch\": \"107-02-002\"\n  }},\n    {\"name\": {\n    \"$nmatch\": \"107-02-003\"\n  }}\n  ],\n  \"type\": \"folder\"\n}).sort({\"$asc\" : [\"created\"]})\n\n"
 
-
-print (payload)
-
+load_dotenv()
+token = os.environ.get('SECRET_TOKEN')
+print("token:::", token)
 headers = {
-    'Authorization': 'Bearer eyJ2ZXIiOiIyIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYiLCJraWQiOiI1bjZUQmVhd2xXT2ZCV3NMeFhuUzJJOGI2Ui0wVC01TUswRVhSajFKdW84In0.eyJzdWIiOiJqZmZlQDAwMFwvdXNlcnNcL2FkbWluIiwic2NwIjoiYXBwbGllZC1wZXJtaXNzaW9uc1wvYWRtaW4gYXBpOioiLCJhdWQiOiIqQCoiLCJpc3MiOiJqZmZlQDAwMCIsImlhdCI6MTYyOTA1MzkwMCwianRpIjoiOGU1ZjFhYTQtZjMwMS00NDcyLWE0MjItMDU4Mjc2NWJkMWIxIn0.AXR0_owoIRgpZ6yBy8q6TElH9JyqPW4fvHEqZL4qeG_dVIsPV7Ii3CUxWuzn5Aa3LoenFjqC4ojBhb_dJyCWpXVxP24wxKtaKNo-huB9en2qPE0CtU4ZZ2QUwj7DThVQph0V0Sx_zl-AVIXK28jebJlZZ_UOiiGGNSk_QpePpueFHo0kJl7kxjgW2NAx4lEPD5Urf-bmt9Er6jX_aiEMvASY3QsOQLE0E-Ho1JSQ-Xtdh4iAW-oRyvqkt3FAMoxhGzEdie4rdD88cLri9Oh8nNzLHXlQJXAK90JMxMRxcTWHpscgkYSFdj7_PbF0JDWOApPoXtVmKu_5Lo63r3-gKQ',
+    'Authorization': f'{token}',
     'Content-Type': 'text/plain'
 }
-
 
 response = requests.request("POST", url, headers=headers, data=payload)
 response_dict = json.loads(response.text)
